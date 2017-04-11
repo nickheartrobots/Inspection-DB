6abe98afc4624ce8ecbb7e1e8b7f167e282cae1e
Nick Clarity <nclarity@deloitte.com>
2017-04-11 15:59:55 -0500

    more validation

--- Web/src/app/inspections/inspectionForm/inspection-form.component.ts
+++ Web/src/app/inspections/inspectionForm/inspection-form.component.ts
@@ -7,15 +7,25 @@ import { InspectionModel } from '../../models/inspection.model';
 })
 export class InspectionFormComponent {
     title: string = 'Inspection Form';
-    states: string[] = ['', 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-                            'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-                            'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-                            'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-                            'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+    states: string[] = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+                        'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+                        'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+                        'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+                        'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
     model = new InspectionModel();
     isError = true;
 
+    isInspectionStateValid: boolean;
+
     onFormChange(): void {
         console.log(this.model.toString());
+    }
+
+    validateInspectionState(event): void {
+        if (this.model.inspectionState === 'default') {
+            this.isInspectionStateValid = false;
+        } else {
+            this.isInspectionStateValid = true;
+        }
     }
 }
