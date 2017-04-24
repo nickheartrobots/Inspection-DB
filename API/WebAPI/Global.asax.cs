8f15d123bcc71de281ac3d653547454618ba7b14
Nick Clarity <nclarity@deloitte.com>
2017-04-24 10:22:05 -0500

    removed initial db schema and created new one

--- API/WebAPI/Global.asax.cs
+++ API/WebAPI/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using WebAPI.Models;
 
 namespace WebAPI
 {
@@ -10,14 +9,6 @@ namespace WebAPI
     {
         protected void Application_Start()
         {
-            //System.Data.Entity.Database.SetInitializer(
-            //    new WebAPIContextInitializer());
-
-            using (var db = new WebAPIContext())
-            {
-                System.Data.Entity.Database.SetInitializer(new WebAPIContextInitializer());
-                db.Database.Initialize(true);
-            }
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
